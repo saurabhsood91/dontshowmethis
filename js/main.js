2be055443b290f47375d1f6834c8fbf6f681854b
Saurabh Sood <saurabhsood91@gmail.com>
2016-11-12 03:33:16 -0800

    hide

--- js/main.js
+++ js/main.js
@@ -1,31 +1,26 @@
 $(function() {
-    // var postElements = $('._1dwg._1w_m');
-    // // console.log(postElements);
-    // // postElements.forEach(function(post) {
-    // //     console.log($(post).find('._4-eo'));
-    // // });
-    // for(var i = 0; i < postElements.length; i++) {
-    //     // console.log($(postElements[i]).find('._4-eo'));
-    //     var a = $(postElements[i]).find('a');
-    //     debugger;
-    //     a.filter(function(val) {
-    //         return $(val).attr('href') !== '#';
-    //     });
-    //     if(a) {
-    //         console.log(a.attr('href'));
-    //     }
-    // }
-    var posts = $('._1dwg._1w_m');
-    // console.log(img);
-
+    var posts = $('.userContentWrapper._5pcr').find('._1dwg._1w_m');
+    console.log(posts.length);
+    // console.log(posts);
     for(var i = 0; i < posts.length; i++) {
         var fbPhotos = $(posts[i]).find('a._4-eo');
         // var links = $(posts[i]).find('a._52c6');
-        var videos = $(posts[i]).find('video');
+        var videos = $(posts[i]).find('img._3chq');
+        var linkImage = $(posts[i]).find('._6l-.__c_').find('img');
         // debugger;
         if(fbPhotos.length > 0) {
-            console.log($(fbPhotos[0]).find('img').attr('src'));
+            var link = $(fbPhotos[0]).find('img').attr('src');
+            $($(fbPhotos[0]).closest('.userContentWrapper._5pcr')).hide()
+            // debugger;
+            console.log(link);
             continue;
+        }
+        if(linkImage.length > 0) {
+            var link = linkImage[0].src;
+            $($(linkImage[0]).closest('.userContentWrapper._5pcr')).hide();
+            // debugger;
+            console.log(link);
+            // continue;
         }
         // if(links.length > 0) {
         //     console.log(links[0].href);
