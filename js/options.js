5dc9bfee28e90ccc926addf83fd785fcc8792f4c
Saurabh Sood <saurabhsood91@gmail.com>
2016-11-12 08:54:24 -0800

    fix options page

--- js/options.js
+++ js/options.js
@@ -1,22 +1,44 @@
-$(function(){
-    console.log('loaded settings');
-    var restoreOptions = function() {
-        chrome.storage.sync.get({
-            'hideNSFW': false
-        }, function(options) {
-            $('#optionsNSFW').val(options.hideNSFW);
+// $(function(){
+//     var restoreOptions = function() {
+//         chrome.storage.sync.get({
+//             'hideNSFW': false
+//         }, function(options) {
+//             document.querySelector('#optionsNSFW').checked = options.hideNSFW;
+//         });
+//     };
+//
+//     $('#optionsNSFW').change(function(){
+//         var nsfwValue = document.querySelector('#optionsNSFW').checked;
+//         chrome.storage.sync.set({
+//             'hideNSFW': nsfwValue
+//         }, function() {
+//             $('#status').html('Option Saved!');
+//         });
+//     });
+//
+//     // Load Settings
+//     document.addEventListener('DOMContentLoaded', restoreOptions, false);
+//
+// });
+//
+// // $(function(){})
+
+var initialize = function() {
+    chrome.storage.sync.get({
+        'hideNSFW': false
+    }, function(options) {
+        document.querySelector('#optionsNSFW').checked = options.hideNSFW;
+    });
+
+    document.querySelector('#optionsNSFW').onchange = function(){
+        var nsfwValue = document.querySelector('#optionsNSFW').checked;
+        chrome.storage.sync.set({
+            'hideNSFW': nsfwValue
+        }, function() {
+            document.querySelector('#status').innerHTML = 'Option Saved!';
         });
     };
 
-    $('#optionsNSFW').change(function(){
-        chrome.storage.sync.set({
-            'hideNSFW': $('#optionsNSFW').val()
-        }, function() {
-            $('#status').html('Option Saved!');
-        });
-    });
+};
 
-    // Load Settings
-    document.addEventListener('DOMContentLoaded', restoreOptions);
-
-})();
+document.addEventListener('DOMContentLoaded', initialize, false);
