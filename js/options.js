d802ffe6ea20c8a34a37f11ed2326491731c8830
Saurabh Sood <saurabhsood91@gmail.com>
2016-11-12 10:02:06 -0800

    add options for sites

--- js/options.js
+++ js/options.js
@@ -25,9 +25,23 @@
 
 var initialize = function() {
     chrome.storage.sync.get({
-        'hideNSFW': false
+        'hideNSFW': false,
+        'sites': []
     }, function(options) {
         document.querySelector('#optionsNSFW').checked = options.hideNSFW;
+
+        if(options.sites.length > 0 && options.sites.indexOf('Facebook') != -1) {
+            document.querySelector('#optionsFacebook').checked = true;
+        } else {
+            document.querySelector('#optionsFacebook').checked = false;
+        }
+
+        if(options.sites.length > 0 && options.sites.indexOf('Twitter') != -1) {
+            document.querySelector('#optionsTwitter').checked = true;
+        } else {
+            document.querySelector('#optionsTwitter').checked = false;
+        }
+
     });
 
     document.querySelector('#optionsNSFW').onchange = function(){
@@ -35,9 +49,28 @@ var initialize = function() {
         chrome.storage.sync.set({
             'hideNSFW': nsfwValue
         }, function() {
-            document.querySelector('#status').innerHTML = 'Option Saved!';
+            document.querySelector('#status').innerHTML = 'NSFW option saved!';
         });
     };
+
+    var saveSiteSettings = function() {
+        var allSites = document.querySelectorAll('.selected-site:checked');
+        var sites = [];
+        for(var i = 0; i < allSites.length; i++) {
+            sites.push(allSites[i].value);
+        }
+        console.log(allSites);
+        chrome.storage.sync.set({
+            'sites': sites
+        }, function() {
+            console.log('sites saved');
+            document.querySelector('#status').innerHTML = 'Site settings saved!';
+        });
+
+    };
+
+    document.querySelector('#optionsTwitter').onchange = saveSiteSettings;
+    document.querySelector('#optionsFacebook').onchange = saveSiteSettings;
 
 };
 
