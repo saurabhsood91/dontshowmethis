9b070aae399f336a2184cc50cbfd85f2a03d95f2
Saurabh Sood <saurabhsood91@gmail.com>
2016-11-12 04:25:25 -0800

    conditions to add class

--- js/main.js
+++ js/main.js
@@ -1,49 +1,83 @@
 $(function() {
 
-    var isNSFW = function(input) {
+    var isNSFW = function(input, callback) {
         Algorithmia.client("simGsutkPeebT3OirQm6PqZCDqg1")
                    .algo("algo://spullara/YahooOpenNSFW/0.1.1")
                    .pipe(input)
-                   .then(function(output) {
-                     console.log(output);
-                   });
+                   .then(callback);
     };
 
-
-    var posts = $('.userContentWrapper._5pcr').find('._1dwg._1w_m');
-    console.log(posts.length);
-    // console.log(posts);
-    for(var i = 0; i < posts.length; i++) {
-        var fbPhotos = $(posts[i]).find('a._4-eo');
-        // var links = $(posts[i]).find('a._52c6');
-        var videos = $(posts[i]).find('img._3chq');
-        var linkImage = $(posts[i]).find('._6l-.__c_').find('img');
-        // debugger;
-        if(fbPhotos.length > 0) {
-            var link = $(fbPhotos[0]).find('img').attr('src');
-            // $($(fbPhotos[0]).closest('.userContentWrapper._5pcr')).hide()
+    var checkForDisallowedContent = function() {
+        var posts = $('.userContentWrapper._5pcr').find('._1dwg._1w_m');
+        console.log(posts.length);
+        // console.log(posts);
+        for(var i = 0; i < posts.length; i++) {
+            var fbPhotos = $(posts[i]).find('a._4-eo');
+            // var links = $(posts[i]).find('a._52c6');
+            var videos = $(posts[i]).find('img._3chq');
+            var linkImage = $(posts[i]).find('._6l-.__c_').find('img');
             // debugger;
-            // console.log(link);
-            isNSFW(link)
-            continue;
+            if(fbPhotos.length > 0) {
+                var link = $(fbPhotos[0]).find('img').attr('src');
+                // $($(fbPhotos[0]).closest('.userContentWrapper._5pcr')).hide()
+                // debugger;
+                // console.log(link);
+                if(!$(posts[i]).hasClass('dont-hide')) {
+                    console.log('fb photos doesnt have class');
+                    isNSFW(link, function(output) {
+                        if(output.result > 0.5) {
+                            $($(fbPhotos[0]).closest('.userContentWrapper._5pcr')).hide()
+                        } else {
+                            console.log('adding class photos');
+                            // debugger;
+                            $(posts[i]).addClass('dont-hide');
+                        }
+                    });
+                    continue;
+                }
+            }
+            if(linkImage.length > 0) {
+                var link = linkImage[0].src;
+                // $($(linkImage[0]).closest('.userContentWrapper._5pcr')).hide();
+                if(!$(posts[i]).hasClass('dont-hide')) {
+                    console.log('link doesnt have class');
+                    isNSFW(link, function(output) {
+                        if(output.result > 0.5) {
+                            $($(linkImage[0]).closest('.userContentWrapper._5pcr')).hide();
+                        } else {
+                            console.log('adding class link');
+                            // debugger;
+                            $(posts[i]).addClass('dont-hide');
+                        }
+                    });
+                }
+                // debugger;
+                // console.log(link);
+                // continue;
+            }
+            // if(links.length > 0) {
+            //     console.log(links[0].href);
+            // }
+            if(videos.length > 0) {
+                // console.log(videos[0].src);
+                var link = videos[0].src;
+                if(!$(posts[i]).hasClass('dont-hide')) {
+                    console.log('videos doesnt have class');
+                    isNSFW(link, function(output) {
+                        if(output.result > 0.5) {
+                            $($(videos[0]).closest('.userContentWrapper._5pcr')).hide();
+                        } else {
+                            console.log('adding class videos');
+                            // debugger;
+                            $(posts[i]).addClass('dont-hide');
+                        }
+                    });
+                }
+                // $($(videos[0]).closest('.userContentWrapper._5pcr')).hide();
+            }
         }
-        if(linkImage.length > 0) {
-            var link = linkImage[0].src;
-            // $($(linkImage[0]).closest('.userContentWrapper._5pcr')).hide();
-            isNSFW(link);
-            // debugger;
-            // console.log(link);
-            // continue;
-        }
-        // if(links.length > 0) {
-        //     console.log(links[0].href);
-        // }
-        if(videos.length > 0) {
-            // console.log(videos[0].src);
-            var link = videos[0].src;
-            isNSFW(link);
-            // $($(videos[0]).closest('.userContentWrapper._5pcr')).hide();
-        }
-    }
+    };
 
+    $(document).scroll(checkForDisallowedContent);
+    checkForDisallowedContent();
 });
