96448c61f9da6d9b5870cdf09d6710be1a4727d9
Saurabh Sood <saurabhsood91@gmail.com>
2016-11-12 03:48:24 -0800

    add algorithmia

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 $(function() {
+
+    var isNSFW = function(input) {
+        Algorithmia.client("simGsutkPeebT3OirQm6PqZCDqg1")
+                   .algo("algo://spullara/YahooOpenNSFW/0.1.1")
+                   .pipe(input)
+                   .then(function(output) {
+                     console.log(output);
+                   });
+    };
+
+
     var posts = $('.userContentWrapper._5pcr').find('._1dwg._1w_m');
     console.log(posts.length);
     // console.log(posts);
@@ -10,24 +21,28 @@ $(function() {
         // debugger;
         if(fbPhotos.length > 0) {
             var link = $(fbPhotos[0]).find('img').attr('src');
-            $($(fbPhotos[0]).closest('.userContentWrapper._5pcr')).hide()
+            // $($(fbPhotos[0]).closest('.userContentWrapper._5pcr')).hide()
             // debugger;
-            console.log(link);
+            // console.log(link);
+            isNSFW(link)
             continue;
         }
         if(linkImage.length > 0) {
             var link = linkImage[0].src;
-            $($(linkImage[0]).closest('.userContentWrapper._5pcr')).hide();
+            // $($(linkImage[0]).closest('.userContentWrapper._5pcr')).hide();
+            isNSFW(link);
             // debugger;
-            console.log(link);
+            // console.log(link);
             // continue;
         }
         // if(links.length > 0) {
         //     console.log(links[0].href);
         // }
         if(videos.length > 0) {
-            console.log(videos[0].src);
-            $($(videos[0]).closest('.userContentWrapper._5pcr')).hide();
+            // console.log(videos[0].src);
+            var link = videos[0].src;
+            isNSFW(link);
+            // $($(videos[0]).closest('.userContentWrapper._5pcr')).hide();
         }
     }
 
