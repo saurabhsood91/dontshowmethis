357d44c065a7bf0a52a439398768bcb74b699fb9
rmaheshkumarblr <rmaheshkumarblr@gmail.com>
2016-11-12 20:49:12 -0800

    Formatting JS again

--- js/main.js
+++ js/main.js
@@ -10,18 +10,13 @@ $(function() {
     var checkForDisallowedContent = function() {
         var posts = $('.userContentWrapper._5pcr:visible').find('._1dwg._1w_m');
         console.log(posts.length);
-        // console.log(posts);
         var postsArray = posts.toArray();
         postsArray.forEach(function(post) {
             var fbPhotos = $(post).find('a._4-eo');
             var videos = $(post).find('img._3chq');
             var linkImage = $(post).find('._6l-.__c_').find('img');
-            // debugger;
             if (fbPhotos.length > 0) {
                 var link = $(fbPhotos[0]).find('img').attr('src');
-                // $($(fbPhotos[0]).closest('.userContentWrapper._5pcr')).hide()
-                // debugger;
-                // console.log(link);
                 if (!$(post).hasClass('dont-hide')) {
                     console.log('fb photos doesnt have class');
                     isNSFW(link, function(output) {
@@ -37,7 +32,6 @@ $(function() {
             }
             if (linkImage.length > 0) {
                 var link = linkImage[0].src;
-                // $($(linkImage[0]).closest('.userContentWrapper._5pcr')).hide();
                 if (!$(post).hasClass('dont-hide')) {
                     console.log('link doesnt have class');
                     isNSFW(link, function(output) {
@@ -50,15 +44,8 @@ $(function() {
                         }
                     });
                 }
-                // debugger;
-                // console.log(link);
-                // continue;
             }
-            // if(links.length > 0) {
-            //     console.log(links[0].href);
-            // }
             if (videos.length > 0) {
-                // console.log(videos[0].src);
                 var link = videos[0].src;
                 if (!$(post).hasClass('dont-hide')) {
                     console.log('videos doesnt have class');
@@ -67,12 +54,10 @@ $(function() {
                         if (output.result > 0.25) {
                             $($(videos[0]).closest('.userContentWrapper._5pcr')).remove();
                         } else {
-                            // console.log('adding class videos');
                             $(post).addClass('dont-hide');
                         }
                     });
                 }
-                // $($(videos[0]).closest('.userContentWrapper._5pcr')).hide();
             }
         });
 
@@ -95,8 +80,6 @@ $(function() {
     }
 
     var microsoftSentimentAnalysis = function(content) {
-        // var params = {}
-        // var output = ""
         var Inputdata = {
             "documents": [{
                 "language": "en",
@@ -107,19 +90,15 @@ $(function() {
 
 
         $.ajax({
-                url: "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment?", //+ $.param(params),
+                url: "https://westus.api.cognitive.microsoft.com/text/analytics/v2.0/sentiment?",
                 beforeSend: function(xhrObj) {
-                    // Request headers
                     xhrObj.setRequestHeader("Content-Type", "application/json");
                     xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "a8d7c1ca87d4433d9bcec1aa74642f4b");
                 },
                 type: "POST",
-                // Request body
                 data: JSON.stringify(Inputdata),
             })
             .done(function(data) {
-                // console.log(data['documents'][0]['score'])
-                // return data['documents'][0]['score']
                 if (!$(content).hasClass('dont-hide')) {
                     console.log(data['documents'][0]['score'])
                     if (data['documents'][0]['score'] < 0.50) {
@@ -131,44 +110,28 @@ $(function() {
                         console.log("Added to dont-hide class")
                     }
                 }
-
-                // alert("success");
             })
-            .fail(function() {
-                // alert("error");
-            });
-        // return output
+            .fail(function() {});
     };
 
     var microsoftVisionTags = function(imageUrl, callback) {
-        // var params = {}
-        // var output = ""
         var Inputdata = {
             "url": imageUrl
         }
 
 
         $.ajax({
-                url: "https://api.projectoxford.ai/vision/v1.0/tag?", //+ $.param(params),
+                url: "https://api.projectoxford.ai/vision/v1.0/tag?",
                 beforeSend: function(xhrObj) {
-                    // Request headers
                     xhrObj.setRequestHeader("Content-Type", "application/json");
                     xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "a04b26d206e2482083db92c60b3b818a");
                 },
                 type: "POST",
-                // Request body
                 data: JSON.stringify(Inputdata),
             })
             .done(callback)
-            .fail(function() {
-                // alert("error");
-            });
-        // return output
+            .fail(function() {});
     };
-
-    // $(document).scroll(function(){
-    //     setTimeout(checkForDisallowedContent, 1500);
-    // });
     var initialize = function() {
         console.log('initializing');
         chrome.storage.sync.get({
@@ -184,7 +147,6 @@ $(function() {
             if (window.location.href.indexOf('https://www.facebook.com') !== -1) {
                 if (sites.length > 0 && sites.indexOf('Facebook') !== -1) {
                     if (optionNfsw) {
-                        // setInterval(checkForDisallowedContent, 5000);
                         checkForDisallowedContent();
                         var photoList = $('._4-u2._24on._5t27._4-u8').find('img').toArray();
                         photoList.forEach(function(img) {
@@ -202,121 +164,98 @@ $(function() {
                     }
                     if (optionNegative) {
                         console.log('removing negative content');
-                        // setInterval(function() {
                         var posts = $('.userContentWrapper._5pcr:visible').find('p').toArray();
                         posts.forEach(function(paragraph) {
                             microsoftSentimentAnalysis(paragraph)
                         })
-                        // }, 5000);
                     }
 
                     if (avoidTags.length > 0) {
                         var posts = $('.userContentWrapper._5pcr:visible').find('._1dwg._1w_m');
                         console.log(posts.length);
-                        // console.log(posts);
                         var postsArray = posts.toArray();
                         postsArray.forEach(function(post) {
                             var fbPhotos = $(post).find('a._4-eo');
                             var videos = $(post).find('img._3chq');
                             var linkImage = $(post).find('._6l-.__c_').find('img');
-                            // debugger;
                             if (fbPhotos.length > 0) {
                                 var link = $(fbPhotos[0]).find('img').attr('src');
-                                // $($(fbPhotos[0]).closest('.userContentWrapper._5pcr')).hide()
-                                // debugger;
-                                // console.log(link);
-                                // setInterval(function() {
-                                    if (!$(post).hasClass('dont-hide')) {
-                                        console.log('fb photos doesnt have class');
-                                        microsoftVisionTags(link, function(data) {
-                                            data['tags'].every(function(tag) {
-                                                if (avoidTags.indexOf(tag.name) !== -1 && tag.confidence > 0.5) {
-                                                    console.log(tag.name);
-                                                    $($(fbPhotos[0]).closest('.userContentWrapper._5pcr')).remove();
-                                                    console.log("The Tag has to be avoided")
-                                                    return false;
-                                                } else {
-                                                    console.log(tag.name);
-                                                    console.log("The Tag has to be printed");
-                                                    $(post).addClass('dont-hide');
-                                                }
-                                            });
-                                        });
-                                    }
-                                // }, 5000);
-                            }
-                            if (linkImage.length > 0) {
-                                var link = linkImage[0].src;
-                                // setInterval(function() {
-                                    if (!$(post).hasClass('dont-hide')) {
-                                        console.log('link doesnt have class');
-                                        microsoftVisionTags(link, function(data) {
-                                            data['tags'].every(function(tag) {
-                                                if (avoidTags.indexOf(tag.name) != -1 && tag.confidence > 0.5) {
-                                                    console.log(tag.name);
-                                                    $($(linkImage[0]).closest('.userContentWrapper._5pcr')).remove();
-                                                    console.log("The Tag has to be avoided");
-                                                    return false;
-                                                } else {
-                                                    console.log(tag.name);
-                                                    console.log("The Tag has to be printed");
-                                                    $(post).addClass('dont-hide');
-                                                }
-                                            });
-                                        });
-                                    }
-                                // }, 5000);
-
-                                // debugger;
-                                // console.log(link);
-                                // continue;
-                            }
-                            // if(links.length > 0) {
-                            //     console.log(links[0].href);
-                            // }
-                            if (videos.length > 0) {
-                                // console.log(videos[0].src);
-                                var link = videos[0].src;
-                                // setInterval(function() {
-                                    if (!$(post).hasClass('dont-hide')) {
-                                        console.log('videos doesnt have class');
-                                        microsoftVisionTags(link, function(data) {
-                                            data['tags'].every(function(tag) {
-                                                if (avoidTags.indexOf(tag.name) != -1 && tag.confidence > 0.5) {
-                                                    console.log(tag.name);
-                                                    $($(videos[0]).closest('.userContentWrapper._5pcr')).remove();
-                                                    console.log("The Tag has to be avoided");
-                                                    return false;
-                                                } else {
-                                                    console.log(tag.name);
-                                                    console.log("The Tag has to be printed");
-                                                    $(post).addClass('dont-hide');
-                                                }
-                                            });
-                                        });
-                                    }
-                                // }, 5000);
-                            }
-                        });
-                        var photoList = $('._4-u2._24on._5t27._4-u8').find('img').toArray();
-                        photoList.forEach(function(img) {
-                            // setInterval(function() {
-                                if (!$(img).hasClass('dont-hide')) {
-                                    microsoftVisionTags(img.src, function(data) {
+                                if (!$(post).hasClass('dont-hide')) {
+                                    console.log('fb photos doesnt have class');
+                                    microsoftVisionTags(link, function(data) {
                                         data['tags'].every(function(tag) {
-                                            if (avoidTags.indexOf(tag.name) != -1 && tag.confidence > 0.5) {
+                                            if (avoidTags.indexOf(tag.name) !== -1 && tag.confidence > 0.5) {
                                                 console.log(tag.name);
-                                                $(img).remove();
+                                                $($(fbPhotos[0]).closest('.userContentWrapper._5pcr')).remove();
+                                                console.log("The Tag has to be avoided")
                                                 return false;
                                             } else {
                                                 console.log(tag.name);
-                                                console.log("The Tag has to be printed")
-                                                $(img).addClass('dont-hide');
+                                                console.log("The Tag has to be printed");
+                                                $(post).addClass('dont-hide');
                                             }
                                         });
                                     });
                                 }
-                            // }, 5000);
+                            }
+                            if (linkImage.length > 0) {
+                                var link = linkImage[0].src;
+                                if (!$(post).hasClass('dont-hide')) {
+                                    console.log('link doesnt have class');
+                                    microsoftVisionTags(link, function(data) {
+                                        data['tags'].every(function(tag) {
+                                            if (avoidTags.indexOf(tag.name) != -1 && tag.confidence > 0.5) {
+                                                console.log(tag.name);
+                                                $($(linkImage[0]).closest('.userContentWrapper._5pcr')).remove();
+                                                console.log("The Tag has to be avoided");
+                                                return false;
+                                            } else {
+                                                console.log(tag.name);
+                                                console.log("The Tag has to be printed");
+                                                $(post).addClass('dont-hide');
+                                            }
+                                        });
+                                    });
+                                }
+                            }
+                            if (videos.length > 0) {
+                                var link = videos[0].src;
+                                if (!$(post).hasClass('dont-hide')) {
+                                    console.log('videos doesnt have class');
+                                    microsoftVisionTags(link, function(data) {
+                                        data['tags'].every(function(tag) {
+                                            if (avoidTags.indexOf(tag.name) != -1 && tag.confidence > 0.5) {
+                                                console.log(tag.name);
+                                                $($(videos[0]).closest('.userContentWrapper._5pcr')).remove();
+                                                console.log("The Tag has to be avoided");
+                                                return false;
+                                            } else {
+                                                console.log(tag.name);
+                                                console.log("The Tag has to be printed");
+                                                $(post).addClass('dont-hide');
+                                            }
+                                        });
+                                    });
+                                }
+                            }
+                        });
+                        var photoList = $('._4-u2._24on._5t27._4-u8').find('img').toArray();
+                        photoList.forEach(function(img) {
+                            if (!$(img).hasClass('dont-hide')) {
+                                microsoftVisionTags(img.src, function(data) {
+                                    data['tags'].every(function(tag) {
+                                        if (avoidTags.indexOf(tag.name) != -1 && tag.confidence > 0.5) {
+                                            console.log(tag.name);
+                                            $(img).remove();
+                                            return false;
+                                        } else {
+                                            console.log(tag.name);
+                                            console.log("The Tag has to be printed")
+                                            $(img).addClass('dont-hide');
+                                        }
+                                    });
+                                });
+                            }
                         });
                     }
                 }
@@ -324,22 +263,17 @@ $(function() {
                 if (sites.length > 0 && sites.indexOf('Twitter') !== -1) {
                     console.log('running for twitter');
                     if (optionNfsw) {
-                        // setInterval(checkForDisallowedContentTwitter, 5000);
                         checkForDisallowedContentTwitter();
                     }
                     if (optionNegative) {
-                        // setInterval(function() {
-                            var posts = $('.TweetTextSize.js-tweet-text.tweet-text').toArray();
-                            posts.forEach(function(paragraph) {
-                                microsoftSentimentAnalysis(paragraph)
-                            });
-                        // }, 5000);
+                        var posts = $('.TweetTextSize.js-tweet-text.tweet-text').toArray();
+                        posts.forEach(function(paragraph) {
+                            microsoftSentimentAnalysis(paragraph)
+                        });
                     }
                 }
             }
         });
     };
-    // document.addEventListener('DOMContentLoaded', initialize, false);
     setInterval(initialize, 5000);
-
 });
